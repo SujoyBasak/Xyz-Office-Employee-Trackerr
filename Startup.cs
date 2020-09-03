473c508a92863f2289410b76182e02c48626b3dc
SujoyBasak <sujoyr723@gmail.com>
2020-09-03 13:26:27 +0530

    log4net

--- Startup.cs
+++ Startup.cs
@@ -34,12 +34,15 @@ namespace XyzOfficeEmployeeTrackerr
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            loggerFactory.AddLog4Net();
+
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
